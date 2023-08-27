7936d1000e58a3215cdae7a1fa205d282cf4e08a
Alessandra Marinho <ale.aarm@gmail.com>
2023-08-27 17:52:41 +0000

    Teste

--- product-list/myapp/index.js
+++ product-list/myapp/index.js
@@ -4,7 +4,27 @@ const app = express();
 const port = process.env.port || 8080;
 // secure the direct call to the application
 
-app.get('/products', getProducts);
+const passport = require('passport');
+const { JWTStrategy } = require('@sap/xssec');
+const xsenv = require('@sap/xsenv');
+
+// XSUAA Middleware
+passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
+
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
+
+app.get('/products', checkReadScope, getProducts);
+
+// Scope check
+function checkReadScope(req, res, next) {
+	if (req.authInfo.checkLocalScope('read')) {
+		return next();
+	} else {
+    	console.log('Missing the expected scope');
+    	res.status(403).end('Forbidden');
+	}
+}
 
 // Serve static files
 app.use('/', express.static('static/'));
